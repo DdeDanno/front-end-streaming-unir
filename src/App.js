90acc0059d481f83c358b319853c10cdeba39931
Daniel Perez <dano.prez.coa@gmail.com>
2025-01-19 16:37:13 -0600

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          Hola y bienvenidos a React! >:D
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <button type="button" className="btn btn-primary">Primary</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
